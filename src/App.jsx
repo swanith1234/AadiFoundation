f5fdfe8e06164d194f45af774a55ee4e781c1f86
swanith kumar reddy pidugu <146310162+swanith1234@users.noreply.github.com>
2024-06-15 20:41:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
+  name: "Swanith Kumar Reddy Pidugu",
+  title: "Web Developer",
+  email: "swanithpidugu@gmail.com",
+  gitHub: "https://github.com/swanith1234",
+  instagram: "",
   linkedIn: "satyanadella",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
